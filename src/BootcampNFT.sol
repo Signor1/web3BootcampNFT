ea233b28d1b87e57554210fba39c81112f1b3518
signor1 <emmanuelomemgboji@gmail.com>
2025-05-28 14:26:07 +0100

    feat: updated image

--- src/BootcampNFT.sol
+++ src/BootcampNFT.sol
@@ -5,15 +5,21 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract BootcampNFT is ERC721Enumerable, Ownable {
-    constructor() payable ERC721("Anambra Web3 Bootcamp Graduate", "AW3BCG") Ownable(msg.sender) {}
+    constructor()
+        payable
+        ERC721("Anambra Web3 Bootcamp Graduate", "AW3BCG")
+        Ownable(msg.sender)
+    {}
 
     function mint(address recipient) external payable onlyOwner {
         _mint(recipient, totalSupply());
     }
 
-    function batchMint(address[] calldata recipients) external payable onlyOwner {
+    function batchMint(
+        address[] calldata recipients
+    ) external payable onlyOwner {
         uint256 recipientsLength = recipients.length;
-        for (uint256 i; i < recipientsLength;) {
+        for (uint256 i; i < recipientsLength; ) {
             _mint(recipients[i], totalSupply());
             unchecked {
                 ++i;
@@ -21,29 +27,41 @@ contract BootcampNFT is ERC721Enumerable, Ownable {
         }
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
         _requireOwned(tokenId);
-        return string(
-            bytes.concat(
-                "{",
-                '"name": "Certificate of Completion",',
-                '"description": "This certificate is proudly presented by Anambra Web3 Conference for successfully completing the Annual Web3 BootCamp held in March 2025.",',
-                '"image": "ipfs://QmXuUvLsit6jAdBYeN9aJ5Cr1cAcBiPAxeR3VJ1vgS5rhz",',
-                '"youtube_url": "https://www.youtube.com/watch?v=AW48_8vKeCg"',
-                "}"
-            )
-        );
+        return
+            string(
+                bytes.concat(
+                    "{",
+                    '"name": "Certificate of Completion",',
+                    '"description": "This certificate is proudly presented by Anambra Web3 Conference for successfully completing the Annual Web3 BootCamp held in March 2025.",',
+                    '"image": "ipfs://QmRW8UUkFzJ3Auzi5bY18eU8v5TpW8cvvoTms15EECjEnW",',
+                    '"youtube_url": "https://www.youtube.com/watch?v=AW48_8vKeCg"',
+                    "}"
+                )
+            );
     }
 
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721Enumerable) returns (bool) {
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721Enumerable) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
-    function _increaseBalance(address account, uint128 amount) internal override(ERC721Enumerable) {
+    function _increaseBalance(
+        address account,
+        uint128 amount
+    ) internal override(ERC721Enumerable) {
         super._increaseBalance(account, amount);
     }
 
-    function _update(address to, uint256 tokenId, address auth) internal override(ERC721Enumerable) returns (address) {
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721Enumerable) returns (address) {
         return super._update(to, tokenId, auth);
     }
 }
